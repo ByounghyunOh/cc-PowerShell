e6d24395f8b222054096866c6e9865228d631406
Byounghyun Oh <byounghyun.oh@gmail.com>
2018-09-08 19:42:33 -0400

    Add-Printer Script created

---
+++ AddPrinter.ps1
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------
+# Subject: Add Printer Script 
+# Description: All variables need to be chanaged for your needs
+# -------------------------------------------------------------
+
+# Variables
+$strPrintPortBW = "\\print.domainname.ca\myPrint-BW"
+$strPrintPortColour = "\\print.domainname.ca\myPrint-Colour"
+
+# Add Printer Port
+
+# Add Printer
+
